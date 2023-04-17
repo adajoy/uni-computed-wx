7edbf9cb86871f4fa16d2720f33b58f6b3d0f7c5
adajoy <adajoyyy@hotmail.com>
2023-04-17 17:17:08 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ a }}</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    title: 'hello'
+  }),
+  computed: {
+    a() {
+      return this.title + '1'
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
